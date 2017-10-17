cc0d3d7d8b8f8fc43de76905bc026a9d5dd94812
Noamgicquiaud <noam.gic@gmail.com>
2017-10-17 14:05:23 +0200

    Ajout page description

--- portailWeb/routes/index.js
+++ portailWeb/routes/index.js
@@ -18,4 +18,12 @@ router.get('/services',
     }
 );
 
+router.get('/description',
+    function(req, res) {
+        console.log(req.session);
+
+        res.render('description', { title: 'Description de la M2L', user: req.user });
+    }
+);
+
 module.exports = router;
