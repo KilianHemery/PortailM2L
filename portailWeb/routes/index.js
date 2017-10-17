bb884ead1fb4660c1338ebec3656ab8363f91e88
flecasble <francois.lecasble@gmail.com>
2017-10-17 10:50:23 +0400

    page services

--- portailWeb/routes/index.js
+++ portailWeb/routes/index.js
@@ -10,4 +10,12 @@ router.get('/',
     }
 );
 
+router.get('/services',
+    function(req, res) {
+        console.log(req.session);
+
+        res.render('services', { title: 'Services de la M2L', user: req.user });
+    }
+);
+
 module.exports = router;
